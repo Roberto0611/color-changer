cf86770edb7f59726d8246551e3e63a6e84d0bdd
Roberto0611 <ochoacuevasr@gmail.com>
2023-05-25 15:54:20 -0700

    finished!

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-//array de colores
+//Made by: Roberto Ochoa Cuevas
+
+//array with colors
 var colors = ["red", "white", "green", "blue", "purple", "aqua", "gray", "black", "orange", "pink", "yellow", "brown", "cyan", "gold", "indigo", "lavender", "magenta", "maroon", "navy", "olive", "orchid", "plum", "salmon", "teal", "turquoise", "violet", "wheat", "coral", "steelblue", "slategray", "aliceblue", "antiquewhite", "aquamarine", "azure", "beige", "bisque", "blanchedalmond", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "greenyellow", "honeydew", "hotpink", "indianred", "ivory", "khaki", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightpink", "lightsalmon","lime", "silver", "khaki", "navajowhite", "thistle", "mediumaquamarine", "tomato", "darkslategray", "mediumorchid"];
 
-//obtener el div (fondo) , el boton y el H1
+//Get the div (background), the button, and the h1
 var background = document.getElementById('box');
 var button = document.getElementById('button1');
 var text = document.getElementById('text');
 
-//Generar numero para poder seleccionar el color
+//Generate number to select color
 function generarNumeroAleatorio() {
     var numeroAleatorio = Math.floor(Math.random() * colors.length) - 1;
     return numeroAleatorio;
 }
 
-//Funcion para cambiar el color
+//Function to change the color
 function backgroundChange(){
     background.style.backgroundColor = colors[generarNumeroAleatorio()];
     var actualColor = background.style.backgroundColor
@@ -20,5 +22,5 @@ function backgroundChange(){
     console.log(actualColor)
 }
 
-//Event listener al hacer click en el boton
+//Event listener on button.
 button.addEventListener("click", backgroundChange);
