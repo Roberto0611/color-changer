207a24944a82fbfcfdf9eca9a15d93b45867d9a0
Roberto0611 <ochoacuevasr@gmail.com>
2023-05-25 15:48:29 -0700

    100 Colors!!

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //array de colores
-var colors = ["red", "white", "green", "blue", "purple","aqua","gray"];
+var colors = ["red", "white", "green", "blue", "purple", "aqua", "gray", "black", "orange", "pink", "yellow", "brown", "cyan", "gold", "indigo", "lavender", "magenta", "maroon", "navy", "olive", "orchid", "plum", "salmon", "teal", "turquoise", "violet", "wheat", "coral", "steelblue", "slategray", "aliceblue", "antiquewhite", "aquamarine", "azure", "beige", "bisque", "blanchedalmond", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "greenyellow", "honeydew", "hotpink", "indianred", "ivory", "khaki", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightpink", "lightsalmon","lime", "silver", "khaki", "navajowhite", "thistle", "mediumaquamarine", "tomato", "darkslategray", "mediumorchid"];
 
 //obtener el div (fondo) , el boton y el H1
 var background = document.getElementById('box');
@@ -17,6 +17,7 @@ function backgroundChange(){
     background.style.backgroundColor = colors[generarNumeroAleatorio()];
     var actualColor = background.style.backgroundColor
     text.textContent = "Background Color:" + " " + actualColor;
+    console.log(actualColor)
 }
 
 //Event listener al hacer click en el boton
