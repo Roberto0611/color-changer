39876e9acd9b17eb20177f9a755df0b7e30776e1
Roberto0611 <ochoacuevasr@gmail.com>
2023-05-25 15:42:24 -0700

    25/05/2023 03:42pm

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-var Colors = ["red", "white", "green", "blue", "purple","aqua","gray"]
+//array de colores
+var colors = ["red", "white", "green", "blue", "purple","aqua","gray"];
+
+//obtener el div (fondo) , el boton y el H1
+var background = document.getElementById('box');
+var button = document.getElementById('button1');
+var text = document.getElementById('text');
+
+//Generar numero para poder seleccionar el color
+function generarNumeroAleatorio() {
+    var numeroAleatorio = Math.floor(Math.random() * colors.length) - 1;
+    return numeroAleatorio;
+}
+
+//Funcion para cambiar el color
+function backgroundChange(){
+    background.style.backgroundColor = colors[generarNumeroAleatorio()];
+    var actualColor = background.style.backgroundColor
+    text.textContent = "Background Color:" + " " + actualColor;
+}
+
+//Event listener al hacer click en el boton
+button.addEventListener("click", backgroundChange);
